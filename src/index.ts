676cee2fa27e49a271b9cb48b8c40ac43089f8ab
Ferhat <erogluferhat@outlook.com>
2024-05-19 20:41:20 +0300

    chore: Update server initialization to use async/await

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { dataSource } from './config/database';
+import userRouter from './routes/user.router';
 
 dotenv.config();
 
-// import routes
-import userRouter from './routes/user.router';
-
-// create express app
 const app = express();
 app.use(express.json());
-
-// attach routes
 app.use('/users', userRouter);
 
-// initialize the data source
-dataSource
-  .initialize()
-  .then(() => {
+const startServer = async () => {
+  try {
+    await dataSource.initialize();
     const port = process.env.PORT || 3000;
-    // Start the server after the data source has been initialized
     app.listen(port, () => {
       console.log(`Server running on http://localhost:${port}`);
     });
-  })
-  .catch((err: Error) => {
+  } catch (err) {
     console.error(err);
     process.exit(1);
-  });
+  }
+};
+
+startServer();
